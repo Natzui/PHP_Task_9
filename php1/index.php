a464c9c63425efa5c0955fb109415234ccb4a47e
Natzui <acostaalfredodanway1848@gmail.com>
2026-08-26 19:30:40 +0800

    Add files via upload

---
+++ php1/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Output 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>PHP Output No. 1</h1>
+    
+    <fieldset>
+        <legend>This form uses GET request</legend>
+        <form action="redirect.php" method="GET">
+        <table>
+            <tr>
+                <td>First Name</td>
+                <td>
+                    <input type="text" name="fname" placeholder="Enter First Name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Middle Name</td>
+                <td>
+                    <input type="text" name="mname" placeholder="Enter Middle Name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Last Name</td>
+                <td>
+                    <input type="text" name="lname" placeholder="Enter Last Name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Age</td>
+                <td>
+                    <input type="number" name="Age" placeholder="Enter Age" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td>
+                    <select name="Gender" required>
+                        <option value="" disabled selected>Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>
+                    <input type="email" name="Email" placeholder="Enter Email" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Address</td>
+                <td>
+                    <input type="text" name="Address" placeholder="Enter Address" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Contact Number</td>
+                <td>
+                    <input type="number" name="CN" placeholder="Enter Contact Number" required />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Submit Data">
+                    <input type="reset" value="Cancel">
+                </td>
+            </tr>
+        </table>
+        </form>
+    </fieldset>
+    
+    <fieldset style="margin-top: 20px">
+        <legend>This form uses POST request</legend>
+        <form action="redirect.php" method="POST">
+        <table>
+            <tr>
+                <td>First Name</td>
+                <td>
+                    <input type="text" name="fname" placeholder="Enter First Name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Middle Name</td>
+                <td>
+                    <input type="text" name="mname" placeholder="Enter Middle Name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Last Name</td>
+                <td>
+                    <input type="text" name="lname" placeholder="Enter Last Name" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Age</td>
+                <td>
+                    <input type="number" name="Age" placeholder="Enter Age" required />
+                </td>
+            </tr>
+            <tr>
+            <td>Gender</td>
+            <td>
+                <select name="Gender" required>
+                    <option value="" disabled selected>Select Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                </select>
+            </td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>
+                    <input type="email" name="Email" placeholder="Enter Email" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Address</td>
+                <td>
+                    <input type="text" name="Address" placeholder="Enter Address" required />
+                </td>
+            </tr>
+            <tr>
+                <td>Contact Number</td>
+                <td>
+                    <input type="number" name="CN" placeholder="Enter Contact Number" required />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Submit Data">
+                    <input type="reset" value="Cancel">
+                </td>
+            </tr>
+        </table>
+        </form>
+    </fieldset>
+
+</body>
+</html>
---
+++ php1/redirect.php
@@ -0,0 +1,84 @@
+<?php
+    
+    if($_SERVER['REQUEST_METHOD'] === 'GET'){
+        $req_type = '$_GET';
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $req_type = '$_POST';
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Output No. 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Data is sent here, and it is store at <?php echo $req_type; ?> variable</h2>
+    <table>
+        <tr>
+            <td width="120">First Name:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['fname'] : $_POST['fname']; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Middle Name:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['mname'] : $_POST['mname']; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Last Name:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['lname'] : $_POST['lname'];?>
+            </td>
+        </tr>
+        <tr>
+            <td>Age:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['Age'] : $_POST['Age'];?>
+            </td>
+        </tr>
+        <tr>
+            <td>Gender:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['Gender'] : $_POST['Gender'];?>
+            </td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['Email'] : $_POST['Email'];?>
+            </td>
+        </tr>
+        <tr>
+            <td>Address:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['Address'] : $_POST['Address'];?>
+            </td>
+        </tr>
+        <tr>
+            <td>Contact Number:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['CN'] : $_POST['CN'];?>
+            </td>
+        </tr>
+    </table>
+    <br><br>
+    <a href="./">Return to Main Form</a>
+</body>
+</html>
